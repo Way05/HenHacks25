5bcad5330da9a4a21c6880f743d713275f5891a8
Wei-en Tan <wtan235@gmail.com>
2025-03-01 11:56:05 -0500

    setting up gemini api calls

--- henhacks25/src/App.tsx
+++ henhacks25/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { fetchResponse } from './gemini_api';
+
 function App() {
   return (
     <div className="App">
--- henhacks25/src/gemini_api.tsx
+++ henhacks25/src/gemini_api.tsx
@@ -3,14 +3,9 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI("AIzaSyCWpbk1sPa-BlZ_1faY4Oyvr8SpxT6govg");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Explain how AI works";
-
-async function fetchResponse() {
+async function fetchResponse(prompt: string) {
     const result = await model.generateContent(prompt);
     return result;
 }
 
-const response = fetchResponse()
-console.log(response);
-
-export {};
+export { fetchResponse };
