8b3d45e45552f6b18cb802d7759bf9033c5a92cc
Wei-en Tan <wtan235@gmail.com>
2025-03-01 14:36:27 -0500

    removed text box from homepage

--- henhacks25/src/pages/home.tsx
+++ henhacks25/src/pages/home.tsx
@@ -8,18 +8,6 @@ const Home: React.FC = () => {
     <div className="Home">
       <h1>Welcome to the Home Page</h1>
 
-      {/* Form */}
-      <div className="input">
-        <form method="post" onSubmit={handleSubmit}>
-          <label>
-            Text input: <input name="myInput" defaultValue="Some initial value" />
-          </label>
-
-          <button type="reset">Reset form</button>
-          <button type="submit">Submit form</button>
-        </form>
-      </div>
-
       {/* Link to Profile Page */}
       <div className="nav">
         <Link to="/profile">
