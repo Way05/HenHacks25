e0245ad3341900190b0561a48b54ae70387236a5
Wei-en Tan <wtan235@gmail.com>
2025-03-01 13:15:03 -0500

    added form with function

--- henhacks25/src/App.tsx
+++ henhacks25/src/App.tsx
@@ -4,10 +4,33 @@ import './App.css';
 
 import { fetchResponse } from './gemini_api';
 
+function handleSubmit(e: any) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // You can pass formData as a fetch body directly:
+    fetch('/some-api', { method: form.method, body: formData });
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  }
+
 function App() {
   return (
     <div className="App">
-      <textarea name="input"/>
+      <form method="post" onSubmit={handleSubmit}>
+      <label>
+        Text input: <input name="myInput" defaultValue="Some initial value" />
+      </label>
+
+      <button type="reset">Reset form</button>
+      <button type="submit">Submit form</button>
+    </form>
     </div>
   );
 }
