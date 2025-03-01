0a2c5ecae874fac5cacf5cfde453012c8c7bcf1e
Wei-en Tan <wtan235@gmail.com>
2025-03-01 13:01:19 -0500

    removed template site

--- henhacks25/src/App.tsx
+++ henhacks25/src/App.tsx
@@ -7,20 +7,7 @@ import { fetchResponse } from './gemini_api';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea name="input"/>
     </div>
   );
 }
