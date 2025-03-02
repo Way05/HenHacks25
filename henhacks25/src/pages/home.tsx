f534df4646264274e1a335a9a1fcc43f6695b8f1
Brian Tran <briant@udel.edu>
2025-03-02 00:06:57 -0500

    CSS for home page buttons

--- henhacks25/src/pages/home.tsx
+++ henhacks25/src/pages/home.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../App.css";
+import "../CSS/home.css";
 
 const Home: React.FC = () => {
   return (
     <div className="Home">
       <h1>Welcome to the Home Page</h1>
       <img src="/images/mirror.jpg" alt="" />
-      <h1>Welcome to Magic Mirror Mental Health Helper</h1>
 
       {/* Link to Profile Page */}
       <div className="nav">
