240bdbf2604cbe373abcb412eeaf251bd967e56e
Wei-en Tan <wtan235@gmail.com>
2025-03-01 20:57:05 -0500

    gemini output shows

--- henhacks25/src/pages/chat.tsx
+++ henhacks25/src/pages/chat.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { handleSubmit } from '../gemini_api';
 
   const ChatPage: React.FC = () => {
+    const [response, setResponse] = useState("Waiting for input");
+
+    const handleData = (data: any) => {
+      const output = handleSubmit(data).then(function(e) {
+        setResponse(e);
+      });
+    }
+
     return (
       <div className="Magic Mirror">
         <h1>Welcome to the Magic Mirror Chat Page</h1>
   
         {/* Form */}
         <div className="input">
-          <form method="post" onSubmit={handleSubmit}>
+          <form method="post" onSubmit={handleData}>
             <label>
               Text input: <input name="myInput" defaultValue="Some initial value" />
             </label>
@@ -18,6 +26,10 @@ import { handleSubmit } from '../gemini_api';
             <button type="reset">Reset form</button>
             <button type="submit">Submit form</button>
           </form>
+        </div>
+
+        <div>
+          <p>{response}</p>
         </div>
   
         {/* Link to Profile Page */}
