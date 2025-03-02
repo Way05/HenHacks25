305215587489fff5b9fba618a31f7efc9a2ade4e
James Wu <jameswuu@udel.edu>
2025-03-01 23:58:44 -0500

    fixed errors

--- henhacks25/src/pages/chat.tsx
+++ henhacks25/src/pages/chat.tsx
@@ -4,14 +4,17 @@ import "../App.css";
 
 import { handleSubmit } from '../gemini_api';
 
-  const ChatPage: React.FC = () => {
-    const [response, setResponse] = useState("Waiting for input");
+const ChatPage: React.FC = () => {
+  const [response, setResponse] = useState("Waiting for input");
 
-    const handleData = (data: any) => {
-      const output = handleSubmit(data).then(function(e) {
-        setResponse(e);
-      });
-    }
+  const handleData = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = formData.get('myInput');
+    handleSubmit(data).then((e) => {
+      setResponse(e);
+    });
+  };
 
   return (
     <div className="Magic Mirror">
@@ -29,11 +32,7 @@ import { handleSubmit } from '../gemini_api';
           </form>
         </div>
 
-        <div>
-          <p>{response}</p>
-        </div>
-  
-        {/* Link to Profile Page */}
+        {/* Link to Profile and Home Page */}
         <div className="nav">
           <Link to="/">
             <button>Go to Home Page</button>
@@ -42,6 +41,11 @@ import { handleSubmit } from '../gemini_api';
             <button>Go to Profile Page</button>
           </Link>
         </div>
+      </div>
+
+      <div className="response">
+        <p>{response}</p>
+      </div>
     </div>
   );
 };
