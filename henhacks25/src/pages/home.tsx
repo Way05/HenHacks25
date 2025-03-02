b7f453c7dee5cd4207e0ae37874f3e6b0e5e3953
Daven Kaufmann <davenk@udel.edu>
2025-03-01 21:34:23 -0500

    idk what I changed

--- henhacks25/src/pages/home.tsx
+++ henhacks25/src/pages/home.tsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 const Home: React.FC = () => {
   return (
     <div className="Home">
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to Magic Mirror Mental Health Helper</h1>
 
       {/* Link to Profile Page */}
       <div className="nav">
         <Link to="/profile">
-          <button>Go to Profile Page</button>
+          <button>Edit Your Profile</button>
         </Link>
         <Link to="/chat">
-          <button>Go to Chat Page</button>
+          <button>Chat With Magic Mirror</button>
         </Link>
       </div>
     </div>
