711cb011e6aac7c4e16b4c5c25cbe360f55d1154
Daven Kaufmann <davenk@udel.edu>
2025-03-02 10:00:56 -0500

    removed stuff

--- henhacks25/src/pages/chat.tsx
+++ henhacks25/src/pages/chat.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "../CSS/chat.css";
-import { l_inputs } from '../gemini_api';
 
 import { handleSubmit } from '../gemini_api';
 import { getUserInfo } from '../App';
